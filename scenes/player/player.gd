b75df19c82b1c9df60c0a895243442a28709e296
LeonardoYaranga <leonardo.yaranga@gmail.com>
2026-09-09 13:18:16 -0500

    feat: Add player hurt mechanics and animations
    - Implemented a new hurt state for the player character, including a jump effect.
    - Added a timer to manage the duration of the hurt state.
    - Introduced a new sound effect for when the player takes damage.
    - Updated player movement logic to prevent actions while in the hurt state.
    - Created animations for the hurt state and integrated them into the animation tree.
    - Modified the player scene to include a hit area for detecting damage.
    - Adjusted player position and camera limits in the level scene.
    - Added a new Area2D node for the hit detection area in the player scene.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,20 +5,34 @@ extends CharacterBody2D
 @onready var land_sound: AudioStreamPlayer = $LandSound
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var player_cam: Camera2D = $PlayerCam
+@onready var hurt_timer: Timer = $HurtTimer
+@onready var hurt_sound: AudioStreamPlayer = $HurtSound
 
 @export var left_limit_cam: int = -10000000
 @export var top_limit_cam: int = -10000000
 @export var right_limit_cam: int = 10000000
 @export var bottom_limit_cam: int = 10000000
 
+#AnimationTree
+var is_still: bool:
+	get: return is_zero_approx(velocity.x)
+var is_falling: bool:
+	get: return velocity.y > 0
+var is_ground: bool:
+	get: return is_on_floor()
+var is_hurt: bool:
+	get: return _is_hurt
 
+#Constants
 var GRAVITY: float = ProjectSettings.get_setting("physics/2d/default_gravity")
-const RUN_SPEED: float = 300.0
+const RUN_SPEED: float = 200.0
 const JUMP_FORCE: float = -400.0
+const HURT_VELOCITY: Vector2 = Vector2(0, -200)
 
 var _jumped: bool = false
 var _was_on_floor: bool = false
 var _start_position: Vector2 = Vector2.ZERO
+var _is_hurt: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -35,7 +49,6 @@ func set_camera_limits() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump") and is_on_floor():
 		_jumped = true
-		_was_on_floor = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -60,6 +73,7 @@ func flip_sprite() -> void:
 	
 
 func handle_movement() -> void:
+	if _is_hurt: return
 	velocity.x = RUN_SPEED * Input.get_axis("left", "right")
 	if _jumped and is_on_floor():
 		_jumped = false
@@ -68,3 +82,18 @@ func handle_movement() -> void:
 
 func fell_off() -> void:
 	set_position.call_deferred(_start_position)
+
+func apply_hurt_jump() -> void:
+	if _is_hurt: return
+	velocity = HURT_VELOCITY
+	_is_hurt = true
+	hurt_timer.start()
+	hurt_sound.play()
+
+func _on_hit_area_area_entered(area: Area2D) -> void:
+	print("Player HIT")
+	apply_hurt_jump.call_deferred()
+
+
+func _on_hurt_timer_timeout() -> void:
+	_is_hurt = false
